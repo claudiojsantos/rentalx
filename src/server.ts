12b72a5f042eb46361601b796d362f6ddcd00fe1
Cláudio Santos <claudiosantos@sistnet.com.br>
2022-02-10 00:00:19 -0300

    chore: formatting file

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({message: 'Hello World!'});
-})
+app.use(categoriesRoutes);
 
-app.listen(3333, () => {console.log('listening on port 3333')})
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World!' });
+});
+
+app.listen(3333, () => {
+  console.log('listening on port 3333');
+});
