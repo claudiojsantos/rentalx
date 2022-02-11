df2b25c784eafd0209dc11619596e4687a26c674
Cláudio Santos <claudiosantos@sistnet.com.br>
2022-02-10 23:14:41 -0300

    feat: create route category

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(categoriesRoutes);
+app.use('/categories', categoriesRoutes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World!' });
